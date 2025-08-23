9c021f9ffac80a31366c937a457503bef54280d4
shivam <codewithshivam24@gmail.com>
2025-08-24 00:10:35 +0530

    Add initial implementations for hello, logical operator, and arithmetic operations

---
+++ logical-operator.py
@@ -0,0 +1,11 @@
+
+"""
+this program is logical operator using
+python language
+"""
+age = int(input("enter age : "))
+
+if age>=18:
+    print("valid for make Driving License") 
+else:
+    print("Minimum Age allow 18")  
---
+++ operator.py
@@ -0,0 +1,17 @@
+
+'''
+this program is the simple 
+arithematic operation 
+using python language
+'''
+
+
+first_num1 = float(input("Enter the first number : "))
+first_num2 = float(input("Enter the Second number : "))
+print(f"sum of two number is {first_num1+first_num2}")
+print(f"substract of two number is {first_num1-first_num2}")
+print(f"multiply of two number is {first_num1*first_num2}")
+print(f"divisin of two number is {first_num1/first_num2}")
+print(f"division of these floor is {first_num1//first_num2}")
+print(f"Remainder of two number is {first_num1%first_num2}")
+print(f"power of two number is {first_num1**first_num2}")
