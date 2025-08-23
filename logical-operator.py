6c03550739545db220fcae58fd4847a29426a25a
shivam <codewithshivam24@gmail.com>
2025-08-24 00:25:18 +0530

    Refactor logical operator program: clean up code and improve docstring clarity

--- logical-operator.py
+++ logical-operator.py
@@ -3,9 +3,53 @@
 this program is logical operator using
 python language .
 """
-age = int(input("enter age : "))
 
-if age>=18:
-    print("valid for make Driving License") 
+
+
+
+# age = int(input("enter age : "))
+
+# if age>=18:
+#     print("valid for make Driving License") 
+# else:
+#     print("Minimum Age allow 18")  
+
+
+
+
+
+
+
+'''
+this program is the check 
+this data is return true
+ or false
+'''
+
+
+
+x=5
+y=45
+
+if (x<5 and y>=20):
+    print("true")
 else:
-    print("Minimum Age allow 18")  
+    print("false")
+
+
+    # logical or
+
+
+if (x<5 or y>=20):
+    print("true")
+else:
+    print("false")
+
+
+#logical not
+
+
+if (not(x<5 and y<=20)):
+    print("true")
+else:
+    print("false")
