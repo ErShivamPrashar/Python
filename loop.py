5e273d1fae5a0000c1f02773b91e21cd3c7e59e3
shivam <codewithshivam24@gmail.com>
2025-08-25 12:11:59 +0530

    Add arithmetic operations and looping program implementations

---
+++ loop.py
@@ -0,0 +1,15 @@
+'''
+this program is looping 
+using 
+'''
+
+ # print table of any number
+
+table = int(input("Enter the table Number : "))
+j=1
+for i in range(table,(table*10+1),table):
+    print(f" {table} x {j} = {i}")
+    j=j+1
+
+
+  
---
+++ string/print.py
@@ -0,0 +1,60 @@
+# name = "shivam"
+# print(name[0])
+# length = len(name)
+# print(length)
+# for i in range(length):
+#     print(name[i],end="")
+
+#multiple line string
+
+# name = '''
+# hello
+# shivam, whats is 
+# your name
+# '''
+# print(name)
+
+
+
+
+
+# # reverse string
+
+
+# name = "shivam"
+# dub_name = name[::-1]
+# print(name)
+# print(dub_name)
+
+# #slice string
+
+# name = "shivam"
+# slice = name[0:3:1] #0-start,3-stop,1-increment
+# print(slice)
+
+
+#adding string
+
+# name = "shivam"
+# new_name = "hello s"+ name[1:6]
+# print(new_name)
+
+
+
+# #replace
+
+# name = "shivam raj"
+# update = name.replace("raj","singh")
+# print(update)
+
+
+# name = "helloshivam is trim"
+
+# name1 = "osh" in name
+# print(name1)
+
+
+name = "hello"
+# print(f"{name+" " }"* 3)
+# print(name.upper())
+# print(name.lower())
